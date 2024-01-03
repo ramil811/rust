a29c6a032d623c525b4071d96378ff6b56a5fadf
Ramil Srivastava <ramilsrivastava@gmail.com>
2024-01-03 12:31:06 +0530

    flip number updated to also use inbuilt mem swap

--- flip_numbers/src/main.rs
+++ flip_numbers/src/main.rs
@@ -7,6 +7,15 @@ fn main() {
     flip_numbers(&mut a, &mut b);
     println!("a = {:p}, b = {:p}", &a, &b);
     println!("a = {}, b = {}", a, b);
+
+    let mut c: i32 = 2;
+    let mut d: i32 = 4;
+    println!("c = {}, d = {}", c, d);
+    // print memory addresses of c and d
+    println!("c = {:p}, d = {:p}", &c, &d);
+    flip_numbers_inbuilt(&mut c, &mut d);
+    println!("c = {:p}, d = {:p}", &c, &d);
+    println!("c = {}, d = {}", c, d);
 }
 
 // write a function to flip the values of two variables
@@ -15,4 +24,8 @@ fn flip_numbers(num1: &mut i32, num2: &mut i32) {
     let c = *num1;
     *num1 = *num2;
     *num2 = c;
+}
+
+fn flip_numbers_inbuilt(num1: &mut i32, num2: &mut i32) {
+    std::mem::swap(num1, num2);
 }
